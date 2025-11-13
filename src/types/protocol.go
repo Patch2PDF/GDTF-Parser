c92c8e5bbcfd6aa2b7efe2b23f51537dc0219036
Jannik-Hm <jannik.heym@gmx.de>
2025-11-14 00:03:35 +0100

    reference resolving for Protocols

--- src/types/protocol.go
+++ src/types/protocol.go
@@ -1,28 +1,44 @@
 package Types
 
 type Protocol struct {
-	RDM              []RDM
-	ArtNet           []ArtNet
-	SACN             []SACN
-	PosiStageNet     []PosiStageNet
-	OpenSoundControl []OpenSoundControl
-	CITP             []CITP
+	RDM              []*RDM
+	ArtNet           []*ArtNet
+	SACN             []*SACN
+	PosiStageNet     []*PosiStageNet
+	OpenSoundControl []*OpenSoundControl
+	CITP             []*CITP
+}
+
+func (obj *Protocol) ResolveReference() {
+	ResolveReferences(&obj.RDM)
 }
 
 type RDM struct {
 	ManufacturerID   Hex
 	DeviceModelID    Hex
-	SoftwareVersions []RDMSoftwareVersion
+	SoftwareVersions []*RDMSoftwareVersion
+}
+
+func (obj *RDM) ResolveReference() {
+	ResolveReferences(&obj.SoftwareVersions)
 }
 
 type RDMSoftwareVersion struct {
 	Value            Hex
-	DMXPersonalities []RDMDMXPersonality
+	DMXPersonalities []*RDMDMXPersonality
+}
+
+func (obj *RDMSoftwareVersion) ResolveReference() {
+	ResolveReferences(&obj.DMXPersonalities)
 }
 
 type RDMDMXPersonality struct {
 	Value   Hex
 	DMXMode NodeReference[DMXMode] // reference to DMX Mode
+}
+
+func (obj *RDMDMXPersonality) ResolveReference() {
+	obj.DMXMode.Ptr = refPointers.DMXModes[obj.DMXMode.String]
 }
 
 type Map struct {
@@ -32,11 +48,11 @@ type Map struct {
 
 // key: artnet value, value: dmx value
 type ArtNet struct {
-	Maps []Map
+	Maps []*Map
 }
 
 type SACN struct {
-	Maps []Map
+	Maps []*Map
 }
 
 type PosiStageNet struct {
